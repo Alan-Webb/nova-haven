f3d086f007826492de5defca76d817c03c4971ee
Alan Webb <webbalan91939@gmail.com>
2025-11-08 18:02:12 +0000

    Update assets & constants

---
+++ src/constants/index.jsx
@@ -0,0 +1,90 @@
+import logo from "./logo.svg";
+import logo_dark from "./logo_dark.svg";
+import headerImg from "./headerImg.webp";
+import brandImg from "./brandImg.webp";
+import propertyImg1 from "./propertyImg1.webp";
+import propertyImg2 from "./propertyImg2.webp";
+import propertyImg3 from "./propertyImg3.webp";
+import propertyImg4 from "./propertyImg4.webp";
+import propertyImg5 from "./propertyImg5.webp";
+import propertyImg6 from "./propertyImg6.webp";
+import profileImg1 from "./profileImg1.webp";
+import profileImg2 from "./profileImg2.webp";
+import profileImg3 from "./profileImg3.webp";
+
+export const assets = {
+	logo,
+	logo_dark,
+	headerImg,
+	brandImg,
+	propertyImg1,
+	propertyImg2,
+	propertyImg3,
+	propertyImg4,
+};
+
+export const projectsData = [
+	{
+		title: "Skyline Haven",
+		price: "$2,50,000",
+		location: "California",
+		image: propertyImg1,
+	},
+	{
+		title: "Vista Verde",
+		price: "$2,50,000",
+		location: "San Francisco",
+		image: propertyImg2,
+	},
+	{
+		title: "Serenity Suites",
+		price: "$2,50,000",
+		location: "Chicago",
+		image: propertyImg3,
+	},
+	{
+		title: "Central Square",
+		price: "$2,50,000",
+		location: "Los Angeles",
+		image: propertyImg4,
+	},
+	{
+		title: "Vista Verde",
+		price: "$2,50,000",
+		location: "San Francisco",
+		image: propertyImg5,
+	},
+	{
+		title: "Serenity Suites",
+		price: "$2,50,000",
+		location: "Chicago",
+		image: propertyImg6,
+	},
+];
+
+export const testimonialsData = [
+	{
+		name: "Donald Jackman",
+		title: "Marketing Manager",
+		image: profileImg1,
+		alt: "Portrait of Donald Jackman",
+		rating: 5,
+		text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
+	},
+	{
+		name: "Richard Nelson",
+		title: "UI/UX Designer",
+		image: profileImg2,
+		alt: "Portrait of Richard Nelson",
+		rating: 4,
+		text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
+	},
+	{
+		name: "James Washington",
+		title: "Co-Founder",
+		image: profileImg3,
+		alt: "Portrait of James Washington",
+		rating: 5,
+		text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched.",
+	},
+];
