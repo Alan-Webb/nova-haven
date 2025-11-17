2a647954d757958bdd39d4ebe5730ec1e80699fb
Alan Webb <webbalan91939@gmail.com>
2025-11-17 06:57:18 +0000

    Update Header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
+import headerImg from "../assets/headerImg.webp";
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className=" ">
+			<img src={headerImg} className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden" />
+		</div>
+	);
 };
 
 export default Header;
