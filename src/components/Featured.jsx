4b75a0a0794f39222b3a7c1f32de5245f7d5d199
Alan Webb <webbalan91939@gmail.com>
2025-12-01 16:40:22 +0000

    Update assets & Featured comp

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,19 @@
+import Title from "./Title";
+
 const Featured = () => {
-	return <div>Featured</div>;
+	return (
+		<section>
+			<div className="container">
+				{/* Title */}
+				<Title
+					title="Featured Properties"
+					text="Explore our handpicked selection of featured properties. Each listing offers a glimpse into exceptional homes and available throughout the Solar System."
+					link="View All Properties"
+				/>
+				{/* Card */}
+			</div>
+		</section>
+	);
 };
 
 export default Featured;
--- src/constants/data.js
+++ src/constants/data.js
@@ -70,6 +70,135 @@ export const CardItems = [
 	},
 ];
 
+export const featuredItems = [
+	{
+		id: 1,
+		img: "./src/assets/prop-1.webp",
+		title: "Solace Dome Residences – Mars Sector 3",
+		text: "A stunning 4-bedroom, 3-bathroom villa in a peaceful Martian neighborhood...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "4-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "3-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "Villa",
+			},
+		],
+		price: 550000,
+	},
+	{
+		id: 2,
+		img: "./src/assets/prop-2.webp",
+		title: "Helios Station - Solar Lagrange 4",
+		text: "A chic and fully-furnished 2-bedroom apartment with solar views...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "2-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "2-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "Villa",
+			},
+		],
+		price: 550000,
+	},
+	{
+		id: 3,
+		img: "./src/assets/prop-3.webp",
+		title: "The Asimov Station Quarters - Enceladus",
+		text: "An elegant 3-bedroom, 2.5-bathroom domicile in Saturn's moon...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "3-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "3-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "Villa",
+			},
+		],
+		price: 550000,
+	},
+	{
+		id: 4,
+		img: "./src/assets/prop-4.webp",
+		title: "Elysium Ridge Outpost – Titan Colony",
+		text: "Modern 5-bedroom, 4-bathroom cabin with a gorgeous view of Saturn...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "5-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "4-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "Cabin",
+			},
+		],
+		price: 750000,
+	},
+	{
+		id: 5,
+		img: "./src/assets/prop-5.webp",
+		title: "Aetherline Sky Lofts – Venus Aerostat District",
+		text: "Sleek 1-bedroom loft in the heart of Venus cloud district with industrial design elements...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "1-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "1-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "Loft",
+			},
+		],
+		price: 320000,
+	},
+	{
+		id: 6,
+		img: "./src/assets/prop-6.webp",
+		title: "Atlas Peaks Bio-Habitat – Europa",
+		text: "Spacious 4-bedroom, 3-bathroom home with a large backyard and view of Jupiter...",
+		icons: [
+			{
+				icon: "./src/assets/prop-icon-1.svg",
+				label: "4-Bedroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-2.svg",
+				label: "3-Bathroom",
+			},
+			{
+				icon: "./src/assets/prop-icon-3.svg",
+				label: "House",
+			},
+		],
+		price: 495000,
+	},
+];
+
 export const socialIcons = [
 	{
 		icon: FaFacebook,
