cbccdc789ee58cd8b20b1592c3135e1368096312
Alan Webb <webbalan91939@gmail.com>
2025-12-02 03:00:59 +0000

    Update Featured & Title components

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,9 @@
 import Title from "./Title";
+import {featuredItems} from "../constants/data";
 
 const Featured = () => {
 	return (
-		<section>
+		<section className="section">
 			<div className="container">
 				{/* Title */}
 				<Title
@@ -11,6 +12,42 @@ const Featured = () => {
 					link="View All Properties"
 				/>
 				{/* Card */}
+				<div className="mt-10 lg:mt-16">
+					{featuredItems.map((item) => (
+						<div key={item.id} className="border border-stone-300 p-6 rounded-xl">
+							<div>
+								<img src={item.img} alt={item.title} width={354} height={255} className="w-full h-full object-cover rounded-xl"/>
+							</div>
+							{/* Content */}
+							<div className="mt-3.5">
+								<h4>{item.title}</h4>
+								<p>{item.text}</p>
+								<div className="flex items-center flex-wrap">
+									{item.icons.map((item, index) => (
+										<div key={index}>
+											<span>
+												<img
+													src={item.icon}
+													alt="icon"
+													width={20}
+													height={20}
+												/>
+											</span>
+											<p>{item.label}</p>
+										</div>
+									))}
+								</div>
+
+								<div>
+									<p>
+										Price <span>{item.price}</span>
+									</p>
+									<button className="secondary-btn">View All Properties</button>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	);
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,23 @@
-const Title = () => {
-	return <div>Title</div>;
+const Title = ({title, text, link}) => {
+	return (
+		<div>
+			{/* Title */}
+			<div className="flex flex-col justify-between items-center gap-3 text-center my-4">
+				<span className="mb-2.5 block">
+					<img
+						src="./src/assets/title-icon.svg"
+						alt="icon"
+						width={55}
+						height={24}
+					/>
+				</span>
+				<h2>{title}</h2>
+				<p className="mt-3 max-w-[730px]">{text}</p>
+			</div>
+			{/* Button */}
+			<button className="primary-btn mx-auto my-4">{link}</button>
+		</div>
+	);
 };
 
 export default Title;
