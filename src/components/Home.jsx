05ed753d060b9a72cca998e823983d40874fe2ce
Alan Webb <webbalan91939@gmail.com>
2025-12-12 15:11:57 +0000

    Update Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import homeImage from "../assets/homeImg.webp";
 
 const Home = () => {
 	return (
-		<div className="relative h-[100vh]">
+		<div className="relative h-screen">
 			<img src={homeImage} alt="Nova Haven Venus Office" />
-			<div className="absolute inset-0"></div>
+			<div className="absolute inset-0 bg-linear-to-t from-black/50 via-black/30 to-black/50"></div>
 		</div>
 	);
 };
