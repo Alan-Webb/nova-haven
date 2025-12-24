a56fe8cc9743a3c50d7f399db5bf3bb3e1050631
Alan Webb <webbalan91939@gmail.com>
2025-12-24 14:13:37 +0000

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
 const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className="bg-gray-900 text-white py-12">
+			<div className="max-w-7xl mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
+				<div>
+					<h3>Nova Haven Co.</h3>
+					<p>Your Trusted Partner in the Solar System</p>
+				</div>
+
+				<div></div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
